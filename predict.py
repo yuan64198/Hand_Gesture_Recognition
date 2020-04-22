fef5da2e9fb5b6c27a096aebcfdc309940f640c8
ChihYuanHuang <yuan64198@gmail.com>
2020-04-22 11:56:05 +0800

    add padding function to predict

--- predict.py
+++ predict.py
@@ -12,10 +12,13 @@ from checkpoints import load_checkpoint
 MAX_LEN = 51
 
 def predict( arr, checkpoint_path):
+    padding = np.zeros((3, MAX_LEN))
+    padding[:, :len(arr[0])] = arr
+    arr = padding
     model = Gest_CNN()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     _ = load_checkpoint(checkpoint_path, model, optimizer)
-    tensor = torch.from_numpy(np.asarray([arr])).float()
+    tensor = torch.from_numpy(np.asarray([padding])).float()
     output = model(tensor)
     pred = output.argmax(dim=1, keepdim=True)
     return pred.data.numpy()[0][0]+1
